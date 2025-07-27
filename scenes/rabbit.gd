9d5727a2338e9774d96ca1b8af29df6c936da970
Akshith Patil <akshithp13@gmail.com>
2025-07-27 22:18:49 +1000

    updated

--- scenes/rabbit.gd
+++ scenes/rabbit.gd
@@ -2,21 +2,28 @@ extends CharacterBody2D
 
 const GRAVITY : int = 4200
 const JUMP_SPEED : int = -1800
+const FAST_FALL_SPEED : int = 3000  # Speed for fast falling
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
+	
 	if is_on_floor():
 		if not get_parent().game_running:
 			$AnimatedSprite2D.play("idle")
 		else:  
 			$RunCol.disabled = false
-			if Input.is_action_pressed("jump"):
+			if Input.is_action_just_pressed("jump"):
 				velocity.y = JUMP_SPEED
 				$JumpSound.play()
 			else:
 				$AnimatedSprite2D.play("running")
 	else:
-		$AnimatedSprite2D.play("jumping")
+		# When in the air, check for fast fall
+		if Input.is_action_pressed("down"):
+			velocity.y = FAST_FALL_SPEED  # Fast fall down
+			$AnimatedSprite2D.play("down")  # Play the down animation
+		else:
+			$AnimatedSprite2D.play("jumping")
 		
 	move_and_slide()
